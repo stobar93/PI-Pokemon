f190180cab142cd95a9593370b9fde8fc9d09151
stobar93 <sebastiantobarquintero@gmail.com>
2021-09-28 09:43:34 -0500

    DB models OK

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -8,5 +8,38 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    hp:{
+      type: DataTypes.INTEGER,
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+    },
+    defense:{
+      type: DataTypes.INTEGER,
+    },
+    speed:{
+      type: DataTypes.INTEGER,
+    },
+    height:{
+      type: DataTypes.INTEGER,
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+    }
   });
 };
+
+// [ ] Pokemon con las siguientes propiedades:
+//   - ID (Número de Pokemon) * : No puede ser un ID de un pokemon ya existente en la API pokeapi
+//   - Nombre *
+//   - Vida
+//   - Fuerza
+//   - Defensa
+//   - Velocidad
+//   - Altura
+//   - Peso
---
+++ api/src/models/Type.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('type', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  });
+};
+// - [ ] Tipo con las siguientes propiedades:
+//   - ID
+//   - Nombre
