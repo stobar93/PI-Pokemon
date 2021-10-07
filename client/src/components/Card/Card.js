3cd3f7cdbe6b1c1b139fa92eb9cb914f36510a8a
stobar93 <sebastiantobarquintero@gmail.com>
2021-10-07 02:19:10 -0500

    Create 50%

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,22 +5,18 @@ import Style from "./Card.module.css";
 export default function Card({name, id, img, types}){
     return (
         <div className={Style.Card}>
-            <h3>{name}</h3>
             
+            <span className={Style.span}>{id}</span>
                 <Link to={`/pokemons/${id}`}>
                     <img id={`pokemonsImg${id}`} className={Style.img} src={img} alt={name}/>
                 </Link>
+                <h3>{name}</h3>
             
+                <div>{types && types.map(t=>{
+                    return <button className={`${Style[t]} ${Style.buttonType}`} key={`button${t}`}  value={t}>{t}</button> 
+                }).slice(0,2)}</div>
+                
             
-            <ul className={Style.cardUl}>
-                
-                <li className={Style.cardLi}>ID: {id}</li>
-                
-                <li>Types:</li>
-                <li>{types && types.map(t=>{
-                    return <button key={`button${t}`}  value={t}>{t}</button> 
-                }).slice(0,2)}</li>
-            </ul>
         </div>
     )
 }
