d849b90d7931625a2ee0b8b4412c774230b66c89
stobar93 <sebastiantobarquintero@gmail.com>
2021-12-01 15:39:29 -0500

    Changed cors

--- api/src/app.js
+++ api/src/app.js
@@ -10,6 +10,7 @@ require("./db.js");
 const server = express();
 
 server.name = "API";
+let origin = process.env.NODE_ENV === "production" ? "https://pokeapp.sebastiantobar.dev" : "*"
 
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
@@ -17,7 +18,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", origin); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
